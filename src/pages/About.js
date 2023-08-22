3fa982d94aaa8cff3cf2e45881330aa32badd194
yen Lin <erte00000@gmail.com>
2023-08-22 14:27:11 +0800

    final

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className="about">
       <h1>
         此網站使用react所製作的一頁式網站 ，去fetch 來自pexel
-        的API，並且可以使用關鍵字去搜索特定圖片網站，版面配置等皆使用的是來自講師所提供的資源
+        的API，並且可以使用關鍵字去搜索特定圖片網站
       </h1>
 
       <Link to="https://www.pexels.com/zh-tw/">
